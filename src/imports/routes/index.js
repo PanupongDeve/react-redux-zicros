ab6f158c2b9983b3fa5e4c33c843d9495738d318
PanupongDeve <5635512110@psu.ac.th>
2018-06-29 15:31:52 +0700

    update page not found

--- src/imports/routes/index.js
+++ src/imports/routes/index.js
@@ -2,13 +2,15 @@
 import Login from '@imports/pages/Login';
 import Public from '@imports/HOC/Public';
 import Register from '@imports/pages/Register';
-import Blank from '@imports/pages/Blank'
+import Blank from '@imports/pages/Blank';
+import NotFound from '@imports/pages/NotFound'
 
 const indexRoutes = [
     { path: '/', name: 'Page3', component: Public(Login) },
     { path: '/login', name: 'login', component: Public(Login) },
     { path: '/register', name: 'register', component: Public(Register) },
-    { path: '/blank', name: 'blank', component: Public(Blank)}
+    { path: '/blank', name: 'blank', component: Public(Blank)},
+    { path: '*', name: 'notfound', component: NotFound }
 ];
 
 export default indexRoutes;
